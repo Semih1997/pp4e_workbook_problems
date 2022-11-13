1de89041e6e8223e0d847b223c78996cbb5ca06f
Semih1997 <emiroglusemih97@gmail.com>
2022-11-13 20:03:57 +0300

    Update database_recovery_4_9.py
    Answer

--- database_recovery_4_9.py
+++ database_recovery_4_9.py
@@ -2,9 +2,9 @@ def reinstall(db):
     i = 0
     final_data_base_keys = []
     final_data_base_values = []
-    
+    final_database = {}
     while i in range(len(db)):
-        
+
         if db[i].isalpha():
             name = ""
             i_adding = 0
@@ -14,19 +14,26 @@ def reinstall(db):
                     i_adding += 1
                 else:
                     name = name.lower()
-                    name = name[0].upper()
+                    name = name.capitalize()
                     final_data_base_keys.append(name)
                     i += i_adding
+                    name = ""
                     break
-                    
-        # elif db[i].isnumeric():
-        #     for j in range(len(db[i:])-1):
-        #         if db[i:][j].isnumeric():
-        #             final_data_base_values += db[i][j]
-        #         else:
-        #             i += 1
+
+        elif db[i].isnumeric():
+            num = ""
+            for j in range(500):
+                if db[i:][j].isnumeric():
+                    num += db[i:][j]
+                else:
+                    num = int(num)
+                    final_data_base_values.append(num)
+                    i += 3
+                    break
 
         else:
             i += 1
-    return final_data_base_keys
+    for k in range(len(final_data_base_keys)):
+        final_database[final_data_base_keys[k]] = final_data_base_values[k]
+    return final_database
 print(reinstall("| ahmet : 16 |=| Mehmet : 19 |=| selin : 32 |=| PINAR : 8 |"))
