902b80628f6927ca831722fee1956e65c3529101
Semih1997 <emiroglusemih97@gmail.com>
2022-11-14 06:31:39 +0300

    Evaluate_Attendees_5_2
    Answers

---
+++ File_Handling_5/evaluate_attendees_5_2_extra.py
@@ -0,0 +1,19 @@
+attendees_file = open("attendees_5_2.txt","r")
+key_file = open("key_5_2.txt","r")
+key = key_file.read().split()
+key_file.close()
+notes = []
+for line in attendees_file.readlines():
+    correct = 0
+    wrong = 0
+    name = line.split()[0]
+    answers = line.split()[1:]
+    for i in range(len(key)):
+        if answers[i] == key[i]:
+            correct += 1
+        else:
+            wrong += 1
+    notes.append((name,correct - 0.25 * wrong))
+attendees_file.close()
+sorted_notes = sorted(notes,key=lambda item: item[1],reverse=True)
+print(notes)
