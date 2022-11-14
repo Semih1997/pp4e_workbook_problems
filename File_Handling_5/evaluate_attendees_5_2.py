8f541b0315733868df1c31ca09cf2fb2b349af51
Semih1997 <emiroglusemih97@gmail.com>
2022-11-14 06:34:30 +0300

    Create evaluate_attendees_5_2.py
    Unfinished

---
+++ File_Handling_5/evaluate_attendees_5_2.py
@@ -0,0 +1,24 @@
+attendees_file = open('attendees_5_2.txt','r')
+answers_file = open('key_5_2.txt','r')
+all_lines = []
+student_dict = {}
+
+for line in attendees_file:
+    all_lines.append(line.split())
+    for i in range(len(all_lines)):
+        key_list = []
+        for j in range(len(all_lines[i])):
+            if j != 0:
+                key_list.append(all_lines[i][j])
+        student_dict[all_lines[i][0]] = key_list
+answers_line = []
+for line in answers_file:
+    answers_line.append(line.split())
+print(student_dict)
+print(answers_line)
+
+"""
+    
+    UNFINISH
+    
+"""
