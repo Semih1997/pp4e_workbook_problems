b65816c77d12a27e0c88aa032a21a445cad5b0b5
Semih1997 <emiroglusemih97@gmail.com>
2022-11-15 19:21:54 +0300

    Pass or fail 5.5
    Answer

---
+++ File_Handling_5/pass_or_fail_5_5.py
@@ -0,0 +1,23 @@
+def pass_or_fail(input_txt,output_txt):
+    notes_lines = open(input_txt,"r")
+    pass_or_fail_list = []
+    for line in notes_lines.readlines():
+        name = line.split()[0]
+        sirname = line.split()[1]
+        first_exam = int(line.split()[3])
+        second_exam = int(line.split()[5])
+        last_exam = int(line.split()[7])
+        averaged_note = (first_exam*0.3)+(second_exam*0.3)+(last_exam*0.4)
+        pass_fail = ""
+        if averaged_note >= 60:
+            pass_fail = "Pass"
+        else:
+            pass_fail = "Fail"
+        pass_or_fail_list.append((name,sirname,",",pass_fail))
+    for i in range(len(pass_or_fail_list)):
+        "".join(str(x) for x in pass_or_fail_list[i])
+    print(pass_or_fail_list)
+    with open(output_txt,"w") as output:
+        for line in pass_or_fail_list:
+            output.write(str(line[0])+ " " +str(line[1])+str(line[2])+str(line[3])+"\n")
+pass_or_fail("input_5_5.txt","output_5_5.txt")
