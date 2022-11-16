2e77243a6daeb6fccc43d7b4231e119811050732
Semih1997 <emiroglusemih97@gmail.com>
2022-11-16 18:34:27 +0300

    Fix Operation File
    Answer

--- File_Handling_5/Qfix_operation_file_5_10.py
+++ File_Handling_5/Qfix_operation_file_5_10.py
@@ -1,7 +1,17 @@
 def fix_operation_file(input_text,output_text):
-    line_list = []
-    with open(input_text,"r") as inputs:
-        for line in inputs:
-            line_list.append(line.split())
-    return line_list
+    input_txt = open(input_text,"r")
+    output_txt = open(output_text,"w")
+    seperator = 0
+    process = ""
+    results = []
+    for line in input_txt:
+        process += line.rstrip("\n")
+        seperator += 1
+        if seperator == 3:
+            output_txt.write(process)
+            output_txt.write("\n")
+            results.append(eval(process))
+            seperator = 0
+            process = ""
+    return results
 print(fix_operation_file("inputs_5_10.txt", "output_5_10.txt"))
