08d42da89a39c01a7f83de8f30506870b4e7ee21
Semih1997 <emiroglusemih97@gmail.com>
2022-11-19 13:29:22 +0300

    Railways
    Same with the workbok

---
+++ Scientific_and_Engineering_Libraries/railways.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# function that applies min max normalization to given numpy array
+def min_max_normalization(arr):
+    min_value = arr.min()
+    max_value = arr.max()
+
+    return (arr - min_value) / (max_value - min_value)
+
+# function that plots and saves the figure with given data, title and output path
+def plot_data(x, y, title, output_path):
+    plt.figure()
+    plt.plot(x, y)
+    plt.title(title)
+    plt.savefig(output_path)
+
+raw_data = pd.read_csv("railway.csv")
+
+lengths = np.array(raw_data["Length"], dtype=int)
+passengers = np.array(raw_data["Passenger"], dtype=int)
+
+normalized_lengths = min_max_normalization(lengths)
+normalized_passengers = min_max_normalization(passengers)
+
+plot_data(lengths, passengers, "RAW", "railway_raw.png")
+plot_data(normalized_lengths, normalized_passengers, "NORMALIZED", "railway_normalized.png")
+print(lengths)
